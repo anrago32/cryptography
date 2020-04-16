6c230c9595b4fe3598453951d459a3c4500fedb1
Alex Rago <anrago32@gmail.com>
2020-04-16 14:57:49 -0400

    Create cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -1,1 +1,9 @@
+# Cryptography
+# Basic cryptography implementation written in Python
+# Written by Antony Gordon, Alex Rago, Nick Rock, 2020
 
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
