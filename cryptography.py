d880c94f435a5762e972cb4e8eb0701f95ee7808
Alex Rago <anrago32@gmail.com>
2020-04-16 21:45:24 -0400

    Add encryption and decryption functionality

--- cryptography.py
+++ cryptography.py
@@ -1,27 +1,35 @@
 # Cryptography
-# Basic cryptography implementation written in Python
+# Basic cryptography implementation written in python
 # Written by Antony Gordon, Alex Rago, Nick Rock, 2020
 
 import random
 
+# Euclidean algorithm for greatest common divisor
 def gcd(a, b):
     if b == 0:
         return a
     else:
         return gcd(b, a % b)
 
+# Algorithm to generate relatively prime public key
 def generate_public_key(n, phi):
     e = random.randrange(1, phi)
     while gcd(e, phi) != 1:
         e = random.randrange(1, phi)
     return e
 
+# Extended euclidean algorithm for multiplicative inverse
 def generate_private_key(e, phi):
-    print("Not Defined")
-    return 0
+    d, d_new = 0, 1
+    r, r_new = phi, e
+    while r_new > 0:
+        a = r // r_new
+        d, d_new = d_new, d - a * d_new
+        r, r_new = r_new, r - a * r_new
+    return d % phi
 
 class Node():
-    def __init__(p, q):
+    def __init__(self, p, q):
         n = p * q
         phi = (p - 1) * (q - 1)
         e = generate_public_key(n, phi)
@@ -31,29 +39,25 @@ class Node():
 
     # Encode with private key of sender and then public key of recipient
     def encrypt(self, recipient, text):
-        key1 = self.private_key
-        key2 = recipient.public_key
-        print("Not Defined")
+        (n, e) = recipient.public_key
+        text = (text ** e) % n
         return text
 
     # Decode with private key of recipient and then public key of sender
     def decrypt(self, sender, text):
-        key1 = self.private_key
-        key2 = sender.public_key
-        print("Not Defined")
+        (n, d) = self.private_key
+        text = (text ** d) % n
         return text
 
 def main():
     # Example Usage
-    # p, q = ...
-    # Alice = Node(p, q)
-    # p, q = ...
-    # Bob = Node(p, q)
-    #
-    # message = "Hello, World!"
-    # message = Alice.encrypt(Bob, message)
-    # message = Bob.decrypt(Alice, message)
-    print()
+    Alice = Node(83, 67)
+    Bob = Node(41, 59)
+
+    message = 5
+    message = Alice.encrypt(Bob, message)
+    message = Bob.decrypt(Alice, message)
+    print(message)
 
 if __name__ == "__main__":
     main()
