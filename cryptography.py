59d412ca8e76d0fa3873b911b4674da03e1a8d6f
Alex Rago <anrago32@gmail.com>
2020-04-17 14:27:51 -0400

    Add example

--- cryptography.py
+++ cryptography.py
@@ -4,16 +4,6 @@
 
 import random
 
-# Convert from string to ASCII
-def fromString(sequence):
-    sequence = [ord(c) for c in sequence]
-    return sequence
-
-# Convert to string from ASCII
-def toString(sequence):
-    sequence = [chr(c) for c in sequence]
-    return "".join(sequence)
-
 # Euclidean algorithm for greatest common divisor
 def gcd(a, b):
     if b == 0:
@@ -38,6 +28,16 @@ def generate_private_key(e, phi):
         r, r_new = r_new, r - a * r_new
     return d % phi
 
+# Convert from string to ASCII
+def fromString(sequence):
+    sequence = [ord(c) for c in sequence]
+    return sequence
+
+# Convert to string from ASCII
+def toString(sequence):
+    sequence = [chr(c) for c in sequence]
+    return "".join(sequence)
+
 class Node():
     def __init__(self, p, q):
         n = p * q
@@ -47,29 +47,37 @@ class Node():
         self.public_key = (n, e)
         self.private_key = (n, d)
 
-    # Encode string with public key of recipient
-    def encrypt(self, recipient, sequence):
-        (n, e) = recipient.public_key
-        sequence = fromString(sequence)
-        sequence = [(c ** e) % n for c in sequence]
-        return sequence
+# Encode string with public key of recipient
+def encrypt(recipient, sequence):
+    (n, e) = recipient.public_key
+    sequence = fromString(sequence)
+    sequence = [(c ** e) % n for c in sequence]
+    return sequence
 
-    # Decode with private key of recipient
-    def decrypt(self, sender, sequence):
-        (n, d) = self.private_key
-        sequence = [(c ** d) % n for c in sequence]
-        sequence = toString(sequence)
-        return sequence
+# Decode with private key of recipient
+def decrypt(recipient, sequence):
+    (n, d) = recipient.private_key
+    sequence = [(c ** d) % n for c in sequence]
+    sequence = toString(sequence)
+    return sequence
 
 def main():
     # Using 8-bit primes
     alice = Node(211, 163)
     bob = Node(113, 199)
+
     message = "Hello, World!"
     print("Text Sent\t->\t" + message)
-    message = alice.encrypt(bob, message)
+    message = encrypt(alice, message)
     print("Transmission\t->\t" + toString(message))
-    message = bob.decrypt(alice, message)
+    message = decrypt(alice, message)
+    print("Text Received\t->\t" + message)
+
+    message = "Hello, Bob!"
+    print("\nText Sent\t->\t" + message)
+    message = encrypt(alice, message)
+    print("Transmission\t->\t" + toString(message))
+    message = decrypt(alice, message)
     print("Text Received\t->\t" + message)
 
 if __name__ == "__main__":
